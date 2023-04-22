d2e391337047cc2b502cf0b96098548b2b219965
HallowAz <amigo_honor@mail.ru>
2023-04-22 21:11:48 +0300

    Добавлен контроль пагинации

--- HomeWork/askme/views.py
+++ HomeWork/askme/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from askme.models import *
 import pdb
+import math
 
 def right_tags(request):
     context = {'tags': TAGS}
     return render(request, 'inc/base.html', context)
 
+
 def index(request, page=1):
+  
     questions, pages =  paginate(list(QUESTIONS), page)
     context =  {
         'page_url':'/index',
@@ -17,6 +20,7 @@ def index(request, page=1):
         'current_page': page
         }
     return render(request, 'index.html', context)
+
 
 def question(request, question_id, page=1):
     context = {'question': QUESTIONS[question_id], 'answers': ANSWERS}
@@ -30,11 +34,14 @@ def question(request, question_id, page=1):
         }
     return render(request, 'question.html', context)
 
+
 def login(request):
     return render(request, 'login.html')
 
+
 def ask(request):
     return render(request, 'ask.html')
+
 
 def hot(request, page=1):
     questions, pages =  paginate(list(QUESTIONS), page)
@@ -47,6 +54,7 @@ def hot(request, page=1):
         }
     
     return render(request, 'hot.html', context)
+
 
 def tag(request, tag_name, page=1):
     questions, pages =  paginate(list(QUESTIONS), page)
@@ -61,12 +69,19 @@ def tag(request, tag_name, page=1):
     
     return render(request, 'tag.html', context)
 
+
 def sign_up(request):
     return render(request, 'signup.html')
 
+
 def paginate(object_list, page, per_page=3):
-    if page < 0 or page >= len(object_list):
+    print(page)
+    
+    if page < 1:      
         page = 1
 
+    if page >= len(object_list)/per_page:    
+        page = math.ceil(len(object_list) / per_page)
+        
     p = Paginator(object_list, per_page)
     return p.get_page(page), list(p.get_elided_page_range(page, on_each_side=1, on_ends=1))
